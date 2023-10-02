0338ca60a5ade2aa084e217d34b88c3b27e042dd
Ksingla07 <karan@pallasathene.com>
2023-10-02 21:59:27 +0530

    Initial Neovim Config with NvChad

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua", "go", "sql", "dockerfile" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
